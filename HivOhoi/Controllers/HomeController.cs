a12328d8e8c50847ad162da76421bb47b202b9e9
alexnaev <102225427+alexnaev@users.noreply.github.com>
2022-04-13 16:09:13 +0200

    Programmed app logic and UI

--- HivOhoi/Controllers/HomeController.cs
+++ HivOhoi/Controllers/HomeController.cs
@@ -18,6 +18,54 @@ namespace HivOhoi.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult HivOhoi()
+        {
+            HivOhoiModel model = new();
+
+            model.HivValue = 3;
+            model.OhoiValue = 5;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult HivOhoi(HivOhoiModel hivOhoi)
+        {
+            List<string> hoItems = new();
+            
+            bool hiv;
+            bool ohoi;
+
+            for(int i = 1; i <= 100; i++)
+            {
+                hiv = (i % hivOhoi.HivValue == 0);
+                ohoi = (i % hivOhoi.OhoiValue == 0);
+
+                if (hiv && ohoi)
+                {
+                    hoItems.Add("HivOhoi");
+                }
+                else if (ohoi)
+                {
+                    hoItems.Add("Ohoi");
+                }
+                else if (hiv)
+                {
+                    hoItems.Add("Hiv");
+                }
+                else
+                {
+                    hoItems.Add(i.ToString());
+                }
+            }
+
+            hivOhoi.Result = hoItems;
+
+            return View(hivOhoi);
+        }
+
         public IActionResult Privacy()
         {
             return View();
---
+++ HivOhoi/Models/HivOhoiModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HivOhoi.Models
+{
+    public class HivOhoiModel
+    {
+        public int HivValue { get; set; }
+        public int OhoiValue { get; set; }
+        public List<string> Result { get; set; } = new();
+    }
+}
